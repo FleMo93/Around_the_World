211e7a92dcca043d451239a7a64e0cc2c6d36d66
Florian Hammer <florianhammer@hotmail.de>
2018-06-02 20:45:49 +0200

    Initial commit

---
+++ Assets/Code/scr_Car.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_Car : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _World;
+    [SerializeField]
+    private float _RotationSpeed = 20;
+    [SerializeField]
+    private float _MovementSpeed = 20;
+
+    private Rigidbody myRigidbody;
+
+    void Start ()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    	
+	}
+
+    private void FixedUpdate()
+    {
+        myRigidbody.AddForce(_MovementSpeed * Time.fixedDeltaTime * this.transform.forward);
+        
+    }
+}
---
+++ Assets/Code/scr_CustomGravity.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_CustomGravity : MonoBehaviour {
+
+    [SerializeField]
+    public float _GravityStrength = -9.81f;
+    [SerializeField]
+    private GameObject _GravityCenter;
+
+    private Rigidbody myRigidbody;
+
+	void Start ()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        Vector3 direction = (_GravityCenter.transform.position - this.transform.position).normalized;
+
+        Vector3 velocity = myRigidbody.velocity + (direction * myRigidbody.mass * Time.fixedDeltaTime);
+
+        //velocity.y -= myRigidbody.mass * Time.fixedDeltaTime;
+
+        myRigidbody.velocity = velocity;
+
+        //Vector3 newVelocity = direction * myRigidbody.velocity.magnitude;
+        //myRigidbody.velocity = newVelocity;
+        //myRigidbody.AddForce(_GravityStrength * (direction * -1) * Time.fixedDeltaTime * _Mass);
+
+        //Debug.Log(myRigidbody.velocity.magnitude);
+    }
+}
