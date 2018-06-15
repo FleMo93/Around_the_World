4135d1c0159edfe5c2cb7a560839218a30dc5689
Florian Hammer <florianhammer@hotmail.de>
2018-06-15 22:45:10 +0200

    Working movement!!!!! : ) YES!

--- Assets/Code/scr_Car.cs
+++ Assets/Code/scr_Car.cs
@@ -12,57 +12,30 @@ public class scr_Car : MonoBehaviour {
     private float _MovementSpeed = 20;
     [SerializeField]
     private float _CarHeightOffset = 0.5f;
+    [SerializeField]
+    private Rigidbody _MyRigidbody;
 
-    private Rigidbody myRigidbody;
-
-    void Start ()
-    {
-        myRigidbody = GetComponent<Rigidbody>();
-	}
-	
 	// Update is called once per frame
 	void Update ()
     {
-        SetRotation();
-        
+        //SetRotation();
+        Physics.gravity = (_World.transform.position - this.transform.position).normalized * 100;
 
-	}
+    }
 
     private void FixedUpdate()
     {
-        SetPosition();
-        lastPos = this.transform.position;
-
-        Vector3 velocity = _MovementSpeed * this.transform.forward;
-        myRigidbody.velocity = velocity;
-    }
-
-    private void SetPosition()
-    {
-        float length = (_World.transform.localScale.x / 2) + _CarHeightOffset;
-        Vector3 startPoint = _World.transform.position;
-        Vector3 dir = (_World.transform.position - this.transform.position).normalized * -1;
-
-        Vector3 endPoint = startPoint + (dir * length);
-        this.transform.position = endPoint;
-    }
-
-    Vector3? lastPos = null;
-    private void SetRotation()
-    {
-        Vector3 dir = (_World.transform.position - this.transform.position).normalized;
-
         if (Input.GetKey(KeyCode.A))
         {
-            this.transform.Rotate(this.transform.up, _RotationSpeed * Time.fixedDeltaTime * -1);
+            transform.Rotate(transform.InverseTransformDirection(transform.transform.up), _RotationSpeed * Time.fixedDeltaTime * -1);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            this.transform.Rotate(transform.InverseTransformDirection(transform.up), _RotationSpeed * Time.fixedDeltaTime);
+            transform.Rotate(transform.InverseTransformDirection(transform.transform.up), _RotationSpeed * Time.fixedDeltaTime);
         }
 
-        Quaternion target = Quaternion.FromToRotation(transform.InverseTransformDirection(transform.up) * -1, dir);
-        target *= Quaternion.FromToRotation(transform.InverseTransformDirection(transform.up) * -1, transform.InverseTransformDirection(transform.up) * -1);
-        this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, target, 50);
+        Vector3 fwd = this.transform.forward * _MovementSpeed;
+        Vector3 vel = fwd;
+        _MyRigidbody.velocity = vel;
     }
 }
